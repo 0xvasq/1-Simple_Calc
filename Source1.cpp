bb33adafdce2995df5584d72317a5e0254d634da
0xvasq <jorge.vasquez007@mymdc.net>
2024-02-29 19:25:01 -0500

    Add a switch...case with char

--- Source1.cpp
+++ Source1.cpp
@@ -3,37 +3,54 @@
 using namespace std;
 
 
-void askUserDisplay(double, double, string);
+void askUserDisplay(double, double, char);
 double add(double, double);
 double subtract(double, double);
+double multiply(double, double);
+double divide(double, double);
+void operationSelection(double, double, char);
 
 int main() {
 	double number1 = 0;
 	double number2 = 0;
-	string operation;
+	char operation = 'x';
 
 	askUserDisplay(number1, number2, operation);
-
-
-
 }
 
-void askUserDisplay(double userNum1, double userNum2, string oper) {
 
+
+void askUserDisplay(double userNum1, double userNum2, char oper) {
+
+	cout << "Which operation would you like to perform?" << endl;
+	cin >> oper;
 	cout << "Please enter the first number" << endl;
 	cin >> userNum1;
 	cout << "Please enter the second number" << endl;
 	cin >> userNum2;
-	cout << "Which operation?";
-	cin >> oper;
 
-	if (oper == "+") {
-		cout << add(userNum1, userNum2);
-	}
-	else if (oper == "-") {
-		subtract(userNum1, userNum2);
-	}
-	else {
+	operationSelection(userNum1, userNum2, oper);
+
+}
+
+
+void operationSelection(double a, double b, char select) {
+
+
+	switch (select) {
+	case '+':
+		cout << "Result: " << add(a, b) << endl;
+		break;
+	case '-':
+		cout << "Result: " << subtract(a, b) << endl;
+		break;
+	case '*':
+		cout << "Result: " << multiply(a, b) << endl;
+		break;
+	case '/':
+		cout << "Result: " << divide(a, b) << endl;
+		break;
+	default:
 		cout << "try again";
 	}
 }
@@ -51,5 +68,12 @@ double multiply(double a, double b) {
 }
 
 double divide(double a, double b) {
-	return a / b;
+	if (b == 0) {
+		cout << "Cannot divide by zero" << endl;
+		return 0;
+	}
+	else {
+		return a / b;
+	}
 }
+
